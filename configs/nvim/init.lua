55495d7d212037f20218b2a107e7269ea92ffe06
embe221ed <baniak996@gmail.com>
2022-08-18 05:47:27 +0200

    update nvim and add Solana template

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -10,7 +10,30 @@ require('nvim-tree').setup {
   open_on_tab = false,
   open_on_setup = true,
   open_on_setup_file = true,
+  view = {
+	adaptive_size = true,
+	centralize_selection = false,
+	width = 40,
+	height = 30,
+	hide_root_folder = false,
+	side = "left",
+	preserve_window_proportions = false,
+	number = false,
+	relativenumber = false,
+	signcolumn = "yes",
+	mappings = {
+	  custom_only = false,
+	  list = {
+		-- user mappings go here
+	  },
+	},
+  }
 }
+-- -- -- auto_close working implementation
+vim.api.nvim_create_autocmd('BufEnter', {
+    command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+    nested = true,
+})
 
 -- -- auto-pairs
 require('nvim-autopairs').setup {}
@@ -115,7 +138,53 @@ lsp.clangd.setup(
     flags = lsp_flags,
   })
 )
+-- -- -- tsserver
+lsp.tsserver.setup(
+  coq.lsp_ensure_capabilities({
+    on_attach = on_attach,
+    flags = lsp_flags,
+  })
+)
+-- -- -- rls
+lsp.rls.setup(
+  coq.lsp_ensure_capabilities({
+	settings = {
+	  rust = {
+		unstable_features = true,
+		build_on_save = false,
+		all_features = true,
+	  },
+	},
+  })
+)
 
 -- INITIALIZE MATERIAL SCHEME
 vim.g.material_style = "palenight"
 vim.cmd 'colorscheme material'
+
+-- SETUP INDENT
+vim.opt.termguicolors = true
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+    show_current_context = true,
+    show_current_context_start = true,
+    space_char_blankline = " ",
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+}
--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -7,32 +7,33 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use {
-    'kyazdani42/nvim-tree.lua',					-- filesystem navigation
-    requires = 'kyazdani42/nvim-web-devicons'			-- filesystem icons
+    'kyazdani42/nvim-tree.lua',									-- filesystem navigation
+    requires = 'kyazdani42/nvim-web-devicons'					-- filesystem icons
   }
-  use 'marko-cerovac/material.nvim'				-- material theme plugin
+  use 'marko-cerovac/material.nvim'								-- material theme plugin
   use {
-    'nvim-treesitter/nvim-treesitter',				-- tree-sitter functionality and highlighting
+    'nvim-treesitter/nvim-treesitter',							-- tree-sitter functionality and highlighting
     run = ':TSUpdate'
   }
-  use 'p00f/nvim-ts-rainbow'					-- rainbow parentheses
+  use 'p00f/nvim-ts-rainbow'									-- rainbow parentheses
   use {
-    'nvim-lualine/lualine.nvim',				-- status line
+    'nvim-lualine/lualine.nvim',								-- status line
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }	-- filesystem icons
   }
-  use 'neovim/nvim-lspconfig'					-- Configurations for Nvim LSP
+  use 'neovim/nvim-lspconfig'									-- Configurations for Nvim LSP
   use {
-    'ms-jpq/coq_nvim',						-- Fast as FUCK and loads of features
+    'ms-jpq/coq_nvim',											-- Fast as FUCK and loads of features
     branch = 'coq'
   }
   use {
-    'RRethy/vim-illuminate',					-- highlight word under cursor
+    'RRethy/vim-illuminate',									-- highlight word under cursor
     requires = 'neovim/nvim-lspconfig'
   }
   use {
-    'akinsho/bufferline.nvim',					-- tabline for nvim
+    'akinsho/bufferline.nvim',									-- tabline for nvim
     tag = "v2.*",
     requires = 'kyazdani42/nvim-web-devicons'
   }
-  use "windwp/nvim-autopairs"					-- auto-pairs
+  use "windwp/nvim-autopairs"									-- auto-pairs
+  use "lukas-reineke/indent-blankline.nvim"						-- indent blankline
 end)
