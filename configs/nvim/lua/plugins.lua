150605df773938b3226fff90aad9ac61dc6be7d8
embe221ed <baniak996@gmail.com>
2023-06-23 13:49:46 +0200

    remove invalid shortcuts

--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -103,18 +103,18 @@ return require("packer").startup(function(use)
             action = 'Telescope find_files',
             key = 'f',
           },
-          {
-            desc = ' Apps',
-            group = 'DiagnosticHint',
-            action = 'Telescope app',
-            key = 'a',
-          },
-          {
-            desc = ' dotfiles',
-            group = 'Number',
-            action = 'Telescope dotfiles',
-            key = 'd',
-          },
+          -- {
+          --   desc = ' Apps',
+          --   group = 'DiagnosticHint',
+          --   action = 'Telescope app',
+          --   key = 'a',
+          -- },
+          -- {
+          --   desc = ' dotfiles',
+          --   group = 'Number',
+          --   action = 'Telescope dotfiles',
+          --   key = 'd',
+          -- },
         },
       },
     }
--- configs/nvim/plugin/packer_compiled.lua
+++ configs/nvim/plugin/packer_compiled.lua
@@ -115,7 +115,7 @@ _G.packer_plugins = {
     url = "https://github.com/hrsh7th/cmp-vsnip"
   },
   ["dashboard-nvim"] = {
-    config = { "\27LJ\2\n�\3\0\0\6\0\r\0\0216\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0034\4\5\0005\5\a\0>\5\1\0045\5\b\0>\5\2\0045\5\t\0>\5\3\0045\5\n\0>\5\4\4=\4\v\3=\3\f\2B\0\2\1K\0\1\0\vconfig\rshortcut\1\0\4\ngroup\vNumber\bkey\6d\tdesc\17 dotfiles\vaction\23Telescope dotfiles\1\0\4\ngroup\19DiagnosticHint\bkey\6a\tdesc\r Apps\vaction\18Telescope app\1\0\6\ngroup\nLabel\ficon_hl\14@variable\tdesc\nFiles\ticon\t \bkey\6f\vaction\25Telescope find_files\1\0\4\ngroup\14@property\bkey\6u\tdesc\16󰊳 Update\vaction\16Lazy update\16week_header\1\0\0\1\0\1\venable\2\1\0\1\ntheme\nhyper\nsetup\14dashboard\frequire\0" },
+    config = { "\27LJ\2\n�\2\0\0\6\0\v\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0034\4\3\0005\5\a\0>\5\1\0045\5\b\0>\5\2\4=\4\t\3=\3\n\2B\0\2\1K\0\1\0\vconfig\rshortcut\1\0\6\ticon\t \ngroup\nLabel\bkey\6f\vaction\25Telescope find_files\ficon_hl\14@variable\tdesc\nFiles\1\0\4\vaction\16Lazy update\ngroup\14@property\bkey\6u\tdesc\16󰊳 Update\16week_header\1\0\0\1\0\1\venable\2\1\0\1\ntheme\nhyper\nsetup\14dashboard\frequire\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
