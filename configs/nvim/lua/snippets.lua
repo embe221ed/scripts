45fb6b1124dd29e20709d158a67c8d4a60490b3f
embe221ed <baniak996@gmail.com>
2023-08-21 17:32:57 +0200

    add custom snippets

--- configs/nvim/lua/snippets.lua
+++ configs/nvim/lua/snippets.lua
@@ -4,21 +4,49 @@ local t   = ls.text_node
 local i   = ls.insert_node
 local f   = ls.function_node
 
+
+-- args is a table, where 1 is the text in Placeholder 1, 2 the text in
+-- placeholder 2,...
+local function copy(args)
+	return args[1]
+end
+
+-- LuaSnip custom snippets
+-- -- Move
 ls.add_snippets("move", {
   s("test", {
-    -- Simple static text.
-    t("#[test]"),
-    -- function, first parameter is the function, second the Placeholders
-    -- whose text it gets as input.
-    -- f(copy, 2),
+    -- simple text with a linebreak (`"\t"`)
+    t("#[test]", "\t"),
+    -- text
+    t("// Parameters: "),
+    -- function, first parameter is the function, second the placeholder's index
+    -- whose text it gets as input. 
+    f(copy, 2),
+    -- text
     t({ "", "public fun " }),
-    -- Placeholder/Insert.
+    -- placeholder/insert
     i(1),
+    -- text
     t("("),
-    -- Linebreak
+    -- placeholder/insert
+    i(2, "val: u64"),
+    -- text + linebreak
     t({ ") {", "\t" }),
-    -- Last Placeholder, exit Point of the snippet.
+    -- last placeholder, exit point of the snippet (idx: 0)
     i(0),
+    -- text
     t({ "", "}" }),
+  }),
+})
+
+-- -- Python
+ls.add_snippets("python", {
+  s("remote", {
+    -- simple text
+    t("HOST, PORT = \""),
+    -- text
+    i(1),
+    -- text
+    t("\".split()"),
   }),
 })
