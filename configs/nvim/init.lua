d7c578686a1c1dd285b2729f11509408c2e1c942
embe221ed <baniak996@gmail.com>
2023-08-21 16:25:37 +0200

    Add snippet mappings

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -238,6 +238,17 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 require("luasnip.loaders.from_vscode").lazy_load()
 
+-- -- LuaSnip keymaps
+vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if luasnip.choice_active() then
+		luasnip.change_choice(1)
+	end
+end, {silent = true})
+
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
