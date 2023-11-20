5ad22850c277f452712c3abefd205d6407d61f58
embe221ed <baniak996@gmail.com>
2023-11-20 11:36:27 +0100

    improve the function to not fully overwrite the previous config

--- configs/nvim/lua/functions.lua
+++ configs/nvim/lua/functions.lua
@@ -4,46 +4,26 @@ vim.api.nvim_create_user_command(
   function(opts)
     local palette = require("catppuccin.palettes").get_palette("macchiato")
     assert(#opts.fargs > 0, "invalid params number: " .. #opts.fargs .. ", at least 1 argument required")
-    if #opts.fargs == 2 then
-      local name = opts.fargs[2]
-      require("no-neck-pain").setup({
-        buffers = {
-          colors = {
-            -- Hexadecimal color code to override the current background color of the buffer. (e.g. #24273A)
-            -- Transparent backgrounds are supported by default.
-            --- @type string?
-            background = palette.crust,
-            -- Brighten (positive) or darken (negative) the side buffers background color. Accepted values are [-1..1].
-            --- @type integer
-            -- blend = -0.2,
-          },
-          scratchPad = {
-            -- When `true`, automatically sets the following options to the side buffers:
-            -- - `autowriteall`
-            -- - `autoread`.
-            --- @type boolean
-            enabled = true,
-            -- set to `nil` to default 
-            -- to current working directory
-            location = nil,
-            -- The name of the generated file. See `location` for more information.
-            --- @type string
-            --- @example: `no-neck-pain-left.norg`
-            fileName = name,
-          },
-          bo = {
-            filetype = "nnp",
-          },
-        },
-      })
-    end
-    vim.cmd("NoNeckPain")
+    -- fetch the NoNeckPain config
+    local config = require("no-neck-pain").config
+    -- set the width
     local ssize = opts.fargs[1]
     local size = assert(tonumber(ssize), "invalid input: " .. ssize .. " is not a number")
-    vim.cmd(string.format("NoNeckPainResize %d", size))
+    config.width = size
+    -- set fileName of buffers if provided
+    if #opts.fargs == 2 then
+      local name = opts.fargs[2]
+      local buffers_config = config.buffers
+      buffers_config.scratchPad.fileName = name
+      for _, side in ipairs({ "left", "right" }) do
+        buffers_config[side].scratchPad.fileName = name
+      end
+    end
+    -- run NoNeckPain command
+    vim.cmd("NoNeckPain")
   end,
   {
     nargs = "+",
-    desc = "Open NoNeckPain layout and run NoNeckPainResize with provided parameter",
+    desc = "Open NoNeckPain layout with provided width [and scratchPad name] parameters",
   }
 )
