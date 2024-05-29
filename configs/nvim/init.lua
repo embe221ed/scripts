7f162806d8d043185390a454d9a49b18cd2e708f
embe221ed <baniak996@gmail.com>
2024-05-29 18:20:58 +0200

    update position of under- and strikethrough lines, change the hover window

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -418,24 +418,13 @@ require("noice").setup {
       telescope = { pattern = "^:%s*Tel?e?s?c?o?p?e?%s+", icon = "", lang = "vim" },
     },
   },
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-    hover = {
-      enabled = false,
-    },
-  },
   -- you can enable a preset for easier configuration
   presets = {
     bottom_search = true, -- use a classic bottom cmdline for search
     command_palette = true, -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    -- lsp_doc_border = true, -- add a border to hover docs and signature help
+    lsp_doc_border = true, -- add a border to hover docs and signature help
   },
   views = {
     cmdline_popup = {
