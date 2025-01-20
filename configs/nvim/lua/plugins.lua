bf18dc40fcdd65165920659b5f6c62e1fe933c2c
embe221ed <baniak996@gmail.com>
2025-01-20 17:21:03 +0100

    comment out unnecessary deps

--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -382,8 +382,8 @@ return require("lazy").setup(
         --- The below dependencies are optional,
         "echasnovski/mini.pick", -- for file_selector provider mini.pick
         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-        "ibhagwan/fzf-lua", -- for file_selector provider fzf
+        -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+        -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
         -- "zbirenbaum/copilot.lua", -- for providers='copilot'
       },
