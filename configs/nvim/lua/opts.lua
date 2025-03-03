2b4746661ef620259393c3c02ce51d00eb4c9cfa
embe221ed <baniak996@gmail.com>
2025-03-03 14:59:52 +0100

    improve statuscolumn

--- configs/nvim/lua/opts.lua
+++ configs/nvim/lua/opts.lua
@@ -1,29 +1,33 @@
 --------------------------------------------------------------------------------
 -- standard options
 --------------------------------------------------------------------------------
-vim.o.shiftwidth        = 2
-vim.o.tabstop           = 2
-vim.o.smartcase         = true
-vim.o.ignorecase        = true
-vim.o.cursorline        = true
-vim.o.number            = true
-vim.o.relativenumber    = true
-vim.o.numberwidth       = 1
-vim.o.statuscolumn      = " %s%l%C "
-vim.o.foldcolumn        = "auto:9"
-vim.o.clipboard         = "unnamedplus"
-vim.o.expandtab         = true
-vim.o.splitright        = true
-vim.o.splitbelow        = true
-vim.o.updatetime        = 500
-vim.o.conceallevel      = 2
+vim.opt.shiftwidth        = 2
+vim.opt.tabstop           = 2
+vim.opt.smartcase         = true
+vim.opt.ignorecase        = true
+vim.opt.cursorline        = true
+vim.opt.number            = true
+vim.opt.relativenumber    = true
+vim.opt.numberwidth       = 1
+vim.opt.statuscolumn      = "%C %s%l "
+vim.opt.foldcolumn        = "auto:9"
+vim.opt.clipboard         = "unnamedplus"
+vim.opt.expandtab         = true
+vim.opt.splitright        = true
+vim.opt.splitbelow        = true
+vim.opt.updatetime        = 500
+vim.opt.conceallevel      = 2
+vim.opt.smoothscroll      = true
 
-vim.o.list              = true
+vim.opt.list              = true
 
 vim.opt.listchars:append "eol:↴"
-vim.opt.fillchars:append "vert:▏,foldopen:┌,foldclose:›"
+vim.opt.fillchars:append "vert:▏,foldopen:┌,foldclose:›,fold: "
 
 vim.opt.termguicolors   = true
 
 vim.lsp.set_log_level("off")
 
+vim.opt.foldexpr = "v:lua.require('utils').foldexpr()"
+vim.opt.foldmethod = "manual"
+vim.opt.foldtext = ""
